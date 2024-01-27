ed583844b39d9e14e729394822e9e1726f0fb280
slemanz <william_sleman@hotmail.com>
2024-01-27 05:29:06 -0300

    gpio(fix): fix error and warnings in build

--- 02_GPIO/Debug/drivers/Src/gpio.d
+++ 02_GPIO/Debug/drivers/Src/gpio.d
@@ -1,3 +1,7 @@
 drivers/Src/gpio.o: ../drivers/Src/gpio.c \
+ /home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h \
+ /home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/gpio.h \
  /home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h
+/home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h:
+/home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/gpio.h:
 /home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h:
