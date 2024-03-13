2f25eb016ed3ecfbb3aaa232990b443aa5e3e06e
slemanz <william_sleman@hotmail.com>
2024-03-13 04:19:15 -0300

    gpio(feat): complete led toggle application

--- 02_GPIO/Debug/Src/01led_toggle.d
+++ 02_GPIO/Debug/Src/01led_toggle.d
@@ -1,1 +1,7 @@
-Src/01led_toggle.o: ../Src/01led_toggle.c
+Src/01led_toggle.o: ../Src/01led_toggle.c \
+ /home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h \
+ /home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/gpio.h \
+ /home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h
+/home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h:
+/home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/gpio.h:
+/home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h:
