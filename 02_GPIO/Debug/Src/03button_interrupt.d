122742b453bb9185e28d04970cc084dc3fd8eeec
slemanz <william_sleman@hotmail.com>
2024-05-12 12:59:30 -0300

    02(delete): delete Debug folder

--- 02_GPIO/Debug/Src/03button_interrupt.d
+++
@@ -1,7 +0,0 @@
-Src/03button_interrupt.o: ../Src/03button_interrupt.c \
- /home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h \
- /home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/gpio.h \
- /home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h
-/home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h:
-/home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/gpio.h:
-/home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h:
--- 02_GPIO/Debug/Src/main.d
+++
@@ -1,7 +0,0 @@
-Src/main.o: ../Src/main.c \
- /home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h \
- /home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/gpio.h \
- /home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h
-/home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h:
-/home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/gpio.h:
-/home/sleman/_git/DRIVER/02_GPIO/drivers/Inc/stm32f401xx.h:
