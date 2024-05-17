f416d694335b6f3c8ecf6f4747ece7412d0dbbfd
slemanz <william_sleman@hotmail.com>
2024-05-17 05:15:21 -0300

    03(feat): add files

--- 03_SPI/Src/main.c
+++ 03_SPI/Src/main.c
@@ -1,8 +1,24 @@
-#include <stdint.h>
+#include "stm32f401xx.h"
+
+void delay(void)
+{
+	for(uint32_t i = 0; i < 500000; i++);
+}
 
 
 int main(void)
 {
-    /* Loop forever */
-	for(;;);
+	pinout_init();
+
+
+
+	while(1)
+	{
+		LED1 = 0;
+		LED2 = 1;
+		delay();
+		LED1 = 1;
+		LED2 = 0;
+		delay();
+	}
 }
