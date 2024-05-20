d39efb28d6e323db21570f330ad28351af4813b6
slemanz <william_sleman@hotmail.com>
2024-05-20 04:17:54 -0300

    03(feat): add API prototypes

--- 03_SPI/Drivers/Inc/spi.h
+++ 03_SPI/Drivers/Inc/spi.h
@@ -31,6 +31,56 @@ typedef struct
 
 
 
+/********************************************************************************************
+ * 								APIs supported by this driver
+ * 					for more information check the function definitions
+ ********************************************************************************************/
+
+
+
+
+/*
+ *		Peripheral Clock Setup
+ */
+
+void SPI_PeriClockControl(SPI_RegDef_t *pGPIOx, uint8_t EnorDi);
+
+
+
+/*
+ * 	Init and De-init
+ */
+
+void SPI_Init(SPI_Handle_t *pSPIHandle);
+void SPI_DeInit(SPI_RegDef_t *pSPIx);
+
+
+/*
+ * 	Data Send and Receive
+ */
+
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len);
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t Len);
+
+
+
+/*
+ * 	IRQ Configuration and ISR handling
+ */
+void SPI_IRQITConfig(uint8_t IRQNumber, uint8_t EnorDi);
+void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority);
+void SPI_IRQHandling(SPI_Handle_t *pHandle);
+
+
+/*
+ * Other Peripheral Control APIs
+ */
+
+
+
+
+
+
 
 
 
