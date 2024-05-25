6eb713c6c323d8f7bb84c7817340e746fda07a30
slemanz <william_sleman@hotmail.com>
2024-05-25 04:09:08 -0300

    spi(feat): begin spi tx application test code

---
+++ 03_SPI/Src/01spi_tx.c
@@ -0,0 +1,29 @@
+#include "stm32f401xx.h"
+
+// test the SPI_sendData API, to send the string
+// "hello world"
+// SPI Master Mode
+// SCLK = max possible
+// DFF = 0 and DFF = 1
+
+/*
+ *  PINs:
+ *  PB12 -> SPI2_NSS
+ *  PB13 -> SPI2_SCK
+ *  PB14 -> SPI2 MISO
+ *  PB15 -> SPI2 MOSI
+ *  Alt function: 5
+ */
+
+
+int main(void)
+{
+
+
+
+
+	while(1)
+	{
+
+	}
+}
