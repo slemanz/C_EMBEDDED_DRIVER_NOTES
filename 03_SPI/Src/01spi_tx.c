db22a360b4e7b3c5e60f98f52994c1bb9c127557
slemanz <william_sleman@hotmail.com>
2024-05-25 04:20:31 -0300

    spi(feat): begin spi init function

--- 03_SPI/Src/01spi_tx.c
+++ 03_SPI/Src/01spi_tx.c
@@ -15,11 +15,41 @@
  *  Alt function: 5
  */
 
+void SPI_GPIOInits(void)
+{
+	GPIO_Handle_t SPIPins;
+
+	SPIPins.pGPIOx = GPIOB;
+	SPIPins.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+	SPIPins.GPIO_PinConfig.GPIO_PinAltFunMode = 5;
+	SPIPins.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	SPIPins.GPIO_PinConfig.GPIO_PinOPType = GPIO_NO_PUPD;
+	SPIPins.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+
+	//SCLK
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+	GPIO_Init(&SPIPins);
+
+	//MOSI
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;
+	GPIO_Init(&SPIPins);
+
+	//MISO
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+	GPIO_Init(&SPIPins);
+
+	//NSS
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GPIO_Init(&SPIPins);
+
+
+}
+
 
 int main(void)
 {
-
-
+	// this funtciton is used to initialize the GPIO pins to behave as SPI2 pins
+	SPI_GPIOInits();
 
 
 	while(1)
