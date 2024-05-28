29c47fc1ac458c752b314c1efc29626725d0e1ed
slemanz <william_sleman@hotmail.com>
2024-05-28 01:46:25 -0300

    spi(feat): add application code

--- 03_SPI/Src/02spi_tx_arduino.c
+++ 03_SPI/Src/02spi_tx_arduino.c
@@ -112,8 +112,15 @@ int main(void)
 		// enable the SPI2 peripheral
 		SPI_PeipheralControl(SPI2, ENABLE);
 
+		// first send length information
+		uint8_t dataLen = strlen(user_data);
+		SPI_SendData(SPI2, &dataLen, 1);
+
 		// to send data
 		SPI_SendData(SPI2, (uint8_t*)user_data, strlen(user_data));
+
+		// lets confirm SPI is not busy
+		while(SPI_GetFlagStatus(SPI2, SPI_BUSY_FLAG));
 
 		// Disable the SPI2 peripheral
 		SPI_PeipheralControl(SPI2, DISABLE);
