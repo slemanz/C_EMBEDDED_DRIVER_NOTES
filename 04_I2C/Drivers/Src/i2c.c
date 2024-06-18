ada198b8c6534892cb118f12165f4e02f544feb1
slemanz <william_sleman@hotmail.com>
2024-06-18 03:45:41 -0300

    i2c(feat): add trise configuration

--- 04_I2C/Drivers/Src/i2c.c
+++ 04_I2C/Drivers/Src/i2c.c
@@ -227,6 +227,21 @@ void I2C_Init(I2C_Handle_t *pI2CHandle)
 	}
 
 	pI2CHandle->pI2C->CCR = tempreg;
+
+	// TRISE Configuration
+	if(pI2CHandle->I2C_Config.I2C_SCLSpeed <= I2C_SCL_SPEED_SM)
+	{
+		// mode is standard mode
+		tempreg = (RCC_GetPCLK1Value() / 1000000U) + 1;
+	}else
+	{
+		// mode is fast mode
+		tempreg = ((RCC_GetPCLK1Value() * 300)/ 1000000U) + 1;
+	}
+
+	pI2CHandle->pI2C->TRISE = (tempreg & 0x3F);
+
+
 }
 
 
