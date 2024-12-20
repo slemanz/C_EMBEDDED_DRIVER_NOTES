f78db31c62514738be35d2d34f6699766f8f8891
slemanz <william_sleman@hotmail.com>
2024-12-20 18:42:58 -0300

    usart(feat): begin usart init

--- 05_USART/Drivers/Src/usart.c
+++ 05_USART/Drivers/Src/usart.c
@@ -75,6 +75,56 @@ void USART_PeripheralControl(USART_RegDef_t *pUSARTx, uint8_t EnorDi)
 	}
 }
 
+/*********************************************************************
+ * @fn      		  - USART_Init
+ *
+ * @brief             -
+ *
+ * @param[in]         -
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -
+ *
+ * @Note              - Resolve all the TODOs
+
+ */
+void USART_Init(USART_Handle_t *pUSARTHandle)
+{
+	// temporary variable
+	uint32_t tempreg = 0;
+
+	// *** Configuration of CR1 **************************************
+	USART_PeriClockControl(pUSARTHandle->pUSARTx, ENABLE);
+
+	// Enable USART Tx and Rx engines
+	if(pUSARTHandle->USART_Config.USART_Mode == USART_MODE_ONLY_RX)
+	{
+		tempreg |= (1 << USART_CR1_RE);
+	}else if(pUSARTHandle->USART_Config.USART_Mode == USART_MODE_ONLY_TX)
+	{
+		tempreg |= (1 << USART_CR1_TE);
+	}else if(pUSARTHandle->USART_Config.USART_Mode == USART_MODE_TXRX)
+	{
+		tempreg |= ((1 << USART_CR1_RE) | (1 << USART_CR1_TE));
+	}
+
+	tempreg |= pUSARTHandle->USART_Config.USART_WordLength << USART_CR1_M;
+
+	if(pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_EN_EVEN)
+	{
+		tempreg |= (1 << USART_CR1_PCE);
+	}else if(pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_EN_ODD)
+	{
+		tempreg |= (1 << USART_CR1_PCE);
+		tempreg |= (1 << USART_CR1_PS);
+	}
+
+	// program the CR1 register
+	pUSARTHandle->pUSARTx->CR1 = tempreg;
+
+	// *** Configuration of CR2 **************************************
+}
 
 /*********************************************************************
  * @fn      		  - USART_GetFlagStatus
@@ -89,6 +139,8 @@ void USART_PeripheralControl(USART_RegDef_t *pUSARTx, uint8_t EnorDi)
  * @Note              -
 
  */
+
+
 
 uint8_t USART_GetFlagStatus(USART_RegDef_t *pUSARTx, uint8_t FlagName)
 {
