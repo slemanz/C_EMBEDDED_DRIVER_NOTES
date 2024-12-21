5ac49dbb7159aac5d52c0c0ebaf58534bbca8623
slemanz <william_sleman@hotmail.com>
2024-12-21 09:34:48 -0300

    usart(feat): implement send and receive

--- 05_USART/Drivers/Src/usart.c
+++ 05_USART/Drivers/Src/usart.c
@@ -203,6 +203,142 @@ void USART_ClearFlag(USART_RegDef_t *pUSARTx, uint16_t StatusFlagName)
 }
 
 /*********************************************************************
+ * @fn      		  - USART_SendData
+ *
+ * @brief             -
+ *
+ * @param[in]         -
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -
+ *
+ * @Note              -
+
+ */
+void USART_SendData(USART_Handle_t *pUSARTHandle, uint8_t *pTxBuffer, uint32_t Len)
+{
+
+	uint16_t *pdata;
+
+   //Loop over until "Len" number of bytes are transferred
+	for(uint32_t i = 0 ; i < Len; i++)
+	{
+		// wait until TXE flag is set in the SR
+		while(!USART_GetFlagStatus(pUSARTHandle->pUSARTx,USART_FLAG_TXE));
+
+         //Check the USART_WordLength item for 9BIT or 8BIT in a frame
+		if(pUSARTHandle->USART_Config.USART_WordLength == USART_WORDLEN_9BITS)
+		{
+			//if 9BIT, load the DR with 2bytes masking the bits other than first 9 bits
+			pdata = (uint16_t*) pTxBuffer;
+			pUSARTHandle->pUSARTx->DR = (*pdata & (uint16_t)0x01FF);
+
+			//check for USART_ParityControl
+			if(pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_DISABLE)
+			{
+				//No parity is used in this transfer. so, 9bits of user data will be sent
+				// Increment pTxBuffer twice (16 bits)
+				pTxBuffer++;
+				pTxBuffer++;
+			}
+			else
+			{
+				//Parity bit is used in this transfer . so , 8bits of user data will be sent
+				//The 9th bit will be replaced by parity bit by the hardware
+				pTxBuffer++;
+			}
+		}
+		else
+		{
+			//This is 8bit data transfer
+			pUSARTHandle->pUSARTx->DR = (*pTxBuffer  & (uint8_t)0xFF);
+			pTxBuffer++;
+		}
+	}
+
+	//wait till TC flag is set in the SR
+	while( ! USART_GetFlagStatus(pUSARTHandle->pUSARTx,USART_FLAG_TC));
+}
+
+
+/*********************************************************************
+ * @fn      		  - USART_ReceiveData
+ *
+ * @brief             -
+ *
+ * @param[in]         -
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -
+ *
+ * @Note              -
+
+ */
+
+void USART_ReceiveData(USART_Handle_t *pUSARTHandle, uint8_t *pRxBuffer, uint32_t Len)
+{
+   //Loop over until "Len" number of bytes are transferred
+	for(uint32_t i = 0 ; i < Len; i++)
+	{
+		//wait until RXNE flag is set in the SR
+		while(!USART_GetFlagStatus(pUSARTHandle->pUSARTx,USART_FLAG_RXNE));
+
+		//Check the USART_WordLength to decide whether we are going to receive 9bit of data in a frame or 8 bit
+		if(pUSARTHandle->USART_Config.USART_WordLength == USART_WORDLEN_9BITS)
+		{
+			//We are going to receive 9bit data in a frame
+
+			//check are we using USART_ParityControl control or not
+			if(pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_DISABLE)
+			{
+				//No parity is used. so, all 9bits will be of user data
+
+				//read only first 9 bits. so, mask the DR with 0x01FF
+				*((uint16_t*) pRxBuffer) = (pUSARTHandle->pUSARTx->DR  & (uint16_t)0x01FF);
+
+				// increment the pRxBuffer two times
+				pRxBuffer++;
+				pRxBuffer++;
+			}
+			else
+			{
+				//Parity is used, so, 8bits will be of user data and 1 bit is parity
+				 *pRxBuffer = (pUSARTHandle->pUSARTx->DR  & (uint8_t)0xFF);
+				 pRxBuffer++;
+			}
+		}
+		else
+		{
+			//We are going to receive 8bit data in a frame
+
+			//check are we using USART_ParityControl control or not
+			if(pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_DISABLE)
+			{
+				//No parity is used , so all 8bits will be of user data
+
+				//read 8 bits from DR
+				*pRxBuffer = (uint8_t) (pUSARTHandle->pUSARTx->DR  & (uint8_t)0xFF);
+			}
+
+			else
+			{
+				//Parity is used, so , 7 bits will be of user data and 1 bit is parity
+
+				//read only 7 bits , hence mask the DR with 0X7F
+				 *pRxBuffer = (uint8_t) (pUSARTHandle->pUSARTx->DR  & (uint8_t)0x7F);
+
+			}
+
+			//increment the pRxBuffer
+			pRxBuffer++;
+		}
+	}
+
+}
+
+/*********************************************************************
  * @fn      		  - USART_IRQInterruptConfig
  *
  * @brief             -
