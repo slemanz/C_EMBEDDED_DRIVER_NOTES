2592ecf091e0cbdcd457ccfb210466966e8bfd06
slemanz <william_sleman@hotmail.com>
2024-12-21 17:23:10 -0300

    begin final project files

---
+++ FINAL_PROJECT/Src/main.c
@@ -0,0 +1,31 @@
+/*
+ * 02uart_case.c
+ *
+ *  Created on: Dec 21, 2024
+ *      Author: sleman
+ */
+
+
+#include<stdio.h>
+#include<string.h>
+#include "stm32f401xx.h"
+
+
+
+
+
+void delay(void)
+{
+	for(uint32_t i = 0 ; i < 500000/2 ; i ++);
+}
+
+int main(void)
+{
+
+    while(1)
+    {
+
+    }
+
+}
+
