3aee9a2b79f0ad810ff7e664c15cdfbf382cfd7b
slemanz <william_sleman@hotmail.com>
2024-12-23 05:07:08 -0300

    final(feat): begin ds1307 init

--- FINAL_PROJECT/bsp/ds1307.c
+++ FINAL_PROJECT/bsp/ds1307.c
@@ -4,10 +4,24 @@
  *  Created on: Dec 23, 2024
  *      Author: sleman
  */
+#include <string.h>
 #include "ds1307.h"
+
+static void ds1307_i2c_pin_config(void);
+static void ds1307_i2c_config(void);
+
+I2C_Handle_t g_ds1307I2cHandle;
 
 uint8_t ds1307_init(void)
 {
+	// 1. init the i2c pins
+	ds1307_i2c_pin_config();
+
+	// 2. init the i2c peripheral
+	ds1307_i2c_config();
+
+	// 3. Enable the I2C peripheral
+	I2C_PeripheralControl(DS1307_I2C, ENABLE);
 
 }
 
@@ -16,3 +30,47 @@ void ds1407_get_current_time(RTC_time_t *rtc_time);
 
 void ds1407_set_current_date(RTC_date_t *rtc_date);
 void ds1407_get_current_date(RTC_date_t *rtc_date);
+
+static void ds1307_i2c_pin_config(void)
+{
+	GPIO_Handle_t i2c_sda, i2c_scl;
+
+	memset(&i2c_sda, 0, sizeof(i2c_sda));
+	memset(&i2c_scl, 0, sizeof(i2c_scl));
+
+	/*
+	 * I2C1_SCL ==> PB6
+	 * I2C1_SDA ==> PB7
+	 */
+
+	i2c_sda.pGPIOx = DS1307_I2C_GPIO_PORT;
+	i2c_sda.GPIO_PinConfig.GPIO_PinAltFunMode = 4;
+	i2c_sda.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+	i2c_sda.GPIO_PinConfig.GPIO_PinNumber = DS1307_I2C_SDA_PIN;
+	i2c_sda.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_OP;
+	i2c_sda.GPIO_PinConfig.GPIO_PinPuPdControl = DS1307_I2C_PUPD;
+	i2c_sda.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+
+	gpio_init(&i2c_sda);
+
+	i2c_scl.pGPIOx = DS1307_I2C_GPIO_PORT;
+	i2c_scl.GPIO_PinConfig.GPIO_PinAltFunMode = 4;
+	i2c_scl.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+	i2c_scl.GPIO_PinConfig.GPIO_PinNumber = DS1307_I2C_SCL_PIN;
+	i2c_scl.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_OP;
+	i2c_scl.GPIO_PinConfig.GPIO_PinPuPdControl = DS1307_I2C_PUPD;
+	i2c_scl.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+
+	gpio_init(&i2c_scl);
+
+}
+
+static void ds1307_i2c_config(void)
+{
+	g_ds1307I2cHandle.pI2Cx = DS1307_I2C;
+	g_ds1307I2cHandle.I2C_Config.I2C_ACKControl = I2C_ACK_ENABLE;
+	g_ds1307I2cHandle.I2C_Config.I2C_SCLSpeed = DS1307_I2C_SPEED;
+
+	I2C_Init(&g_ds1307I2cHandle);
+
+}
