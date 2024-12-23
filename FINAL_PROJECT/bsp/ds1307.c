37d64484430dfa4209ceefd5d220c0073b434d78
slemanz <william_sleman@hotmail.com>
2024-12-23 04:29:40 -0300

    final(feat): add ds1307 prototypes and defines

--- FINAL_PROJECT/bsp/ds1307.c
+++ FINAL_PROJECT/bsp/ds1307.c
@@ -4,5 +4,15 @@
  *  Created on: Dec 23, 2024
  *      Author: sleman
  */
+#include "ds1307.h"
 
+uint8_t ds1307_init(void)
+{
 
+}
+
+void ds1407_set_current_time(RTC_time_t *rtc_time);
+void ds1407_get_current_time(RTC_time_t *rtc_time);
+
+void ds1407_set_current_date(RTC_date_t *rtc_date);
+void ds1407_get_current_date(RTC_date_t *rtc_date);
