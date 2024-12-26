168515e9df24a18ad85a060072ff540a7dd043d2
slemanz <william_sleman@hotmail.com>
2024-12-26 05:20:03 -0300

    final(feat): improve lcd init

--- FINAL_PROJECT/bsp/lcd.c
+++ FINAL_PROJECT/bsp/lcd.c
@@ -7,6 +7,10 @@
 
 #include "lcd.h"
 
+
+static void write_4_bits(uint8_t value);
+
+
 void lcd_init(void)
 {
 	// 1. configure the gpio pins which are used for lcd connections
@@ -48,4 +52,35 @@ void lcd_init(void)
 	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D7, GPIO_PIN_RESET);
 
 	// 2. Do the lcd initialization
+
+	mdelay(40);
+
+	/* RS = 0, for LCD command */
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_RS, GPIO_PIN_RESET);
+
+	/* RnW = 0, writing to LCD */
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_RW, GPIO_PIN_RESET);
+
+	write_4_bits(0x3);
+
+	mdelay(5);
+
+	write_4_bits(0x3);
+
+	udelay(150);
+
+	write_4_bits(0x3);
+	write_4_bits(0x2);
+}
+
+
+/* write 4 bits of data/command on to D4, D5, D6, D7 lines */
+static void write_4_bits(uint8_t value)
+{
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D4, ((value >> 0) & 0x1) );
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D5, ((value >> 1) & 0x1) );
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D6, ((value >> 2) & 0x1) );
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D7, ((value >> 3) & 0x1) );
+
+	lcd_enable();
 }
